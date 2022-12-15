a64c222d2c2f0dd7a3c3ff2aa06c4237ce05c67c
Thomas <thomasconnerse@gmail.com>
2022-12-14 18:07:19 -0600

    Update NumCharAppearances.java

--- InterviewPrep/NumCharAppearances.java
+++ InterviewPrep/NumCharAppearances.java
@@ -1,3 +1,4 @@
+//Returns number of appearances of a specified character in a specified string
 public static int numCharAppearances(String str, char target){
     int numAppearances=0;
     for(int i = 0; i<str.length(); i++){
