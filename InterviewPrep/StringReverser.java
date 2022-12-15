8d64f4c39acf36e996ace530595634ef7ace2ce5
Thomas <thomasconnerse@gmail.com>
2022-12-14 18:18:31 -0600

    Update StringReverser.java

--- InterviewPrep/StringReverser.java
+++ InterviewPrep/StringReverser.java
@@ -1,4 +1,4 @@
-//Returns a reversed string
+//Q: Write a program that reverses a string
 public static String stringReverser(String str){
     String rts = "";
     for(int i = str.length()-1; i >=0; i--){
