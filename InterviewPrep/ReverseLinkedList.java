eb831b11f09a084312fa9fad1deb7c75823015e4
Thomas <thomasconnerse@gmail.com>
2022-12-14 18:09:12 -0600

    Update ReverseLinkedList.java

--- InterviewPrep/ReverseLinkedList.java
+++ InterviewPrep/ReverseLinkedList.java
@@ -1,3 +1,4 @@
+//Returns a reversed linked list
 public static LinkedList<Integer> reverseLinkedList(LinkedList<Integer> list){
     LinkedList<Integer> reversed = new LinkedList();
     for(int i = list.size()-1;  i>= 0; i--){
