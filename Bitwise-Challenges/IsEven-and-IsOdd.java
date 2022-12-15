05f99dbc04068380e0462925fd3ce66655250c07
Thomas <thomasconnerse@gmail.com>
2022-12-14 18:23:25 -0600

    Create IsEven-and-IsOdd.java

---
+++ Bitwise-Challenges/IsEven-and-IsOdd.java
@@ -0,0 +1,4 @@
+//Q: write a program that can determine whether a number is even or odd using bitwise operations
+
+public static boolean isEven(int num){return(num&1)==0;}
+public static boolean isOdd(int num){return (num&1)==1;}
