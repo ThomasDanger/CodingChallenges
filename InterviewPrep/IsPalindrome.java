7304586f6ccb2da269769acea1a0ea469acd1e74
Thomas <thomasconnerse@gmail.com>
2022-12-14 18:05:30 -0600

    Update IsPalindrome.java

--- InterviewPrep/IsPalindrome.java
+++ InterviewPrep/IsPalindrome.java
@@ -1,4 +1,5 @@
 public static boolean isPalindrome(String str){
+    str=str.toUpperCase();
     for(int i = 0; i<str.length()/2; i++){
         if(str.charAt(i)!=str.charAt(str.length()-1-i))
             return false;
