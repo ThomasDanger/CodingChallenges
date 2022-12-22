b1da6b94a326b49a35581efaedb6a2bf76ae9b78
Thomas <thomas.conner08@gmail.com>
2022-12-22 10:36:55 -0600

    Update RecursiveFactorial.java

--- InterviewPrep/RecursiveFactorial.java
+++ InterviewPrep/RecursiveFactorial.java
@@ -1,6 +1,6 @@
 //Q: Write a program that calculates the factorial of a number using recursion
 public static int factorial(int num){
-    if(num==0)
-        return 1;
+    if(num>=1)
+        return num;
     return num*factorial(num-1);
 }
